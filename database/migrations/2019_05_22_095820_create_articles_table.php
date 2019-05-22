5c0c544ef6eed672acec8fd45db0f43dd775a4bb
vagrant <vagrant@homestead>
2019-05-22 10:12:34 +0000

    3

---
+++ database/migrations/2019_05_22_095820_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('book_id');
+            $table->string('title');
+            $table->string('editor');
+            $table->text('description');
+            $table->string('img');
+            $table->text('try_read_title');
+            $table->text('try_read');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}

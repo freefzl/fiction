fd30b4d1176d104df6fc7a4655245f8f6b626954
freefzl <2053099365@qq.com>
2019-06-10 22:50:52 +0800

    edit

--- routes/web.php
+++ routes/web.php
@@ -3,34 +3,34 @@
 Route::domain('www.xiaoshuo.com')->group(function () {
     Route::get('/','Home\IndexController@index');
 
-    Route::get('/book','Home\BookController@index');
-    Route::get('/huanxiangyinen','Home\BookController@huanxiangyinen');
-    Route::get('/dushizhichang','Home\BookController@dushizhichang');
-    Route::get('/xuanhuanqihuan','Home\BookController@xuanhuanqihuan');
-    Route::get('/gudaiyanqing','Home\BookController@gudaiyanqing');
-    Route::get('/wuxiaxianxia','Home\BookController@wuxiaxianxia');
-    Route::get('/lishijunshi','Home\BookController@lishijunshi');
-    Route::get('/lingyikehuan','Home\BookController@lingyikehuan');
-    Route::get('/youxijingji','Home\BookController@youxijingji');
-    Route::get('/xiandaiyanqing','Home\BookController@xiandaiyanqing');
-    Route::get('/chuanyuezhongsheng','Home\BookController@chuanyuezhongsheng');
-    Route::get('/zongcaihaomen','Home\BookController@zongcaihaomen');
-    Route::get('/qingchunxiaoyuan','Home\BookController@qingchunxiaoyuan');
-    Route::get('/duanpianxiaoshuo','Home\BookController@duanpianxiaoshuo');
-    Route::post('/booksehlf','Home\BookController@booksehlf');
+    Route::get('/book/','Home\BookController@index');
+    Route::get('/huanxiangyinen/','Home\BookController@huanxiangyinen');
+    Route::get('/dushizhichang/','Home\BookController@dushizhichang');
+    Route::get('/xuanhuanqihuan/','Home\BookController@xuanhuanqihuan');
+    Route::get('/gudaiyanqing/','Home\BookController@gudaiyanqing');
+    Route::get('/wuxiaxianxia/','Home\BookController@wuxiaxianxia');
+    Route::get('/lishijunshi/','Home\BookController@lishijunshi');
+    Route::get('/lingyikehuan/','Home\BookController@lingyikehuan');
+    Route::get('/youxijingji/','Home\BookController@youxijingji');
+    Route::get('/xiandaiyanqing/','Home\BookController@xiandaiyanqing');
+    Route::get('/chuanyuezhongsheng/','Home\BookController@chuanyuezhongsheng');
+    Route::get('/zongcaihaomen/','Home\BookController@zongcaihaomen');
+    Route::get('/qingchunxiaoyuan/','Home\BookController@qingchunxiaoyuan');
+    Route::get('/duanpianxiaoshuo/','Home\BookController@duanpianxiaoshuo');
+    Route::post('/booksehlf/','Home\BookController@booksehlf');
     Route::get('/book/{id}','Home\BookController@xq');
 
 
-    Route::get('/article','Home\ArticleController@index');
+    Route::get('/article/','Home\ArticleController@index');
     Route::get('/article/{id}','Home\ArticleController@xq');
 
-    Route::get('/top','Home\TopController@index');
+    Route::get('/top/','Home\TopController@index');
     Route::get('/top/{name}','Home\TopController@type');
 
-    Route::get('/zt','Home\ZtController@index');
+    Route::get('/zt/','Home\ZtController@index');
     Route::get('/zt/{id}','Home\ZtController@ZtXq');
 
-    Route::post('/search','Home\IndexController@Search');
+    Route::post('/search/','Home\IndexController@Search');
 
 
 });

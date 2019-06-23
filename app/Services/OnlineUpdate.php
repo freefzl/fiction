fafe521d3a3955674599314814383547bc68aa7b
freefzl <2053099365@qq.com>
2019-06-23 12:45:05 +0800

    1

--- app/Services/OnlineUpdate.php
+++ app/Services/OnlineUpdate.php
@@ -22,6 +22,7 @@ use GuzzleHttp\Exception\RequestException;
 class OnlineUpdate{
     public static function updateBooks()
     {
+
         $types = NovelType::get();
         foreach ($types as $type){
             $is_type = NovelBook::where(['type_id'=>$type->id])->where(['is_up'=>1])->first();
@@ -29,7 +30,7 @@ class OnlineUpdate{
             if(!$is_type){
 
                 NovelBook::where(['type_id'=>$type->id])->where(['is_up'=>0])->limit(1)->update(['is_up'=>1]);
-                $id = NovelBook::where(['type_id'=>$type->id])->where(['is_up'=>1])->orderBy('id','desc')->limit(1)->first();
+                $id = NovelBook::where(['type_id'=>$type->id])->where(['is_up'=>1])->orderBy('updated_at','desc')->limit(1)->first();
 
                 if($id){
                     NovelChapter::where(['bid'=>$id->id])->update(['is_up'=>1]);
@@ -38,9 +39,9 @@ class OnlineUpdate{
             }
         }
         NovelBook::where(['is_up'=>0])->limit(1)->update(['is_up'=>1]);
-        $id = NovelBook::where(['is_up'=>1])->orderBy('id','desc')->limit(1)->first();
-        
-        if($id){
+
+        $id = NovelBook::where(['is_up'=>1])->orderBy('updated_at','desc')->limit(1)->first();
+        if($id->id){
             NovelChapter::where(['bid'=>$id->id])->update(['is_up'=>1]);
         }
 

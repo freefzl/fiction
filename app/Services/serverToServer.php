633848031cb55293562a725ddd6b1dd479a06f80
freefzl <2053099365@qq.com>
2019-06-29 12:16:53 +0800

    1

--- app/Services/serverToServer.php
+++ app/Services/serverToServer.php
@@ -31,19 +31,20 @@ class serverToServer{
         while (true){
             $id = NovelChapter::orderBy('id','desc')->limit(1)->first();
 //            dd($id->id);
-            if(!$id->id<9214183){
-                $info = DB::connection('mysql208')->select('select * from novel_chapters where id > ? limit 1',[$id->id+1]);
+            if(!$id->id<=9214183){
+                $info = DB::connection('mysql208')->select('select * from novel_chapters where id = ? limit 1',[$id->id+1]);
+                
                 $novel_chapter = new NovelChapter();
-                $novel_chapter->id = $info->id;
-                $novel_chapter->bid = $info->bid;
-                $novel_chapter->chapter_id = $info->chapter_id;
-                $novel_chapter->title = $info->title;
-                $novel_chapter->chapterContent = $info->chapterContent;
-                $novel_chapter->is_pay = $info->is_pay;
-                $novel_chapter->is_page = $info->is_page;
-                $novel_chapter->goId = $info->goId;
-                $novel_chapter->pubdate = $info->pubdate;
-                $novel_chapter->is_up = $info->is_up;
+                $novel_chapter->id = $info[0]->id;
+                $novel_chapter->bid = $info[0]->bid;
+                $novel_chapter->chapter_id = $info[0]->chapter_id;
+                $novel_chapter->title = $info[0]->title;
+                $novel_chapter->chapterContent = $info[0]->chapterContent;
+                $novel_chapter->is_pay = $info[0]->is_pay;
+                $novel_chapter->is_page = $info[0]->is_page;
+                $novel_chapter->goId = $info[0]->goId;
+                $novel_chapter->pubdate = $info[0]->pubdate;
+                $novel_chapter->is_up = $info[0]->is_up;
                 $novel_chapter->save();
             }else{
                 break;

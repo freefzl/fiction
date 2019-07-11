edba9ec1212bba4a04b17fd070223b3a83fe6461
vagrant <vagrant@homestead>
2019-07-11 01:15:39 +0000

    1

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -31,7 +31,7 @@ class IndexController extends Controller
         $types = NovelType::all();
 
         $tj_chapter =  NovelChapter::where(['is_up'=>1])->where(['is_pay'=>0])->where(['goId'=>'0'])->with(['book'])->orderBy('updated_at','desc')->limit(70)->get();
-        if($tj_chapter){
+        if(blank($tj_chapter)){
             $tj_chapter = $tj_chapter->random(3);
         }
 
@@ -74,7 +74,7 @@ class IndexController extends Controller
 
         //小说资讯
         $chapters = NovelChapter::where(['is_up'=>1])->where(['is_pay'=>0])->where(['goId'=>0])->orderBy('updated_at','desc')->limit(70)->get();
-        if($chapters){
+        if(blank($chapters)){
             $chapters = $chapters->random(10);
         }
 
